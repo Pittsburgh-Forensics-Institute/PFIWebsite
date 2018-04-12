d987acf099bba9375cccb0c917739107f5008b86
Akshath Jain <akshath.r.jain@gmail.com>
2018-04-12 11:27:16 -0400

    json updates

--- scripts/importantInfoScripts.js
+++ scripts/importantInfoScripts.js
@@ -5,17 +5,21 @@ Purpose: scripts to future proof the PFI websites - i.e. create script to automa
 */
 
 $(document).ready(function(){
-	$.getJSON("https://raw.githubusercontent.com/akshathjain/PFIWebsite/newchanges/json/importantInfo.json?token=AKI1jC2LxLa8zF8RybKI4Q5-QfhaiOC3ks5a2K-3wA%3D%3D", function(data){
+	$.getJSON("https://raw.githubusercontent.com/akshathjain/PFIWebsite/newchanges/json/importantInfo.json?token=AKI1jDwrxvtLKNy5ezRd9yGm_oqwVmdrks5a2LP_wA%3D%3D", function(data){
 		
 		var yearData = data[data.length - 1]; //get data for current year
+
+		console.log(yearData);
 
 		//assign signup link to button
 		document.getElementById("signup-button").href = yearData.signup;
 
 		//assign dates and times
 		var whenString = "Sessions will be held Monday to Friday from " + yearData.startTime + " - " + yearData.endTime + " starting on " + yearData.startDate + " and ending on " + yearData.endDate + ".";
-
 		document.getElementById("when-description").innerHTML = whenString;
+
+		//assign location to link and map
+		
 
 	});
 });
