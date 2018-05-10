35e59d48b9f1e2fe827bf0f664a86cfc2f63c347
akshathjain <akshath.r.jain@gmail.com>
2018-05-10 16:42:07 -0400

    just a test

--- scripts/importantInfoScripts.js
+++ scripts/importantInfoScripts.js
@@ -4,20 +4,20 @@ Date: 4/12/18
 Purpose: scripts to future proof the PFI websites - i.e. create script to automatically update the new dates on a yearly basis
 */
 
-$(document).ready(function(){
-	$.getJSON("https://pghforensics.org/json/importantInfo.json", function(data){
-		
+$(document).ready(function () {
+	$.getJSON("https://pghforensics.org/json/importantInfo.json", function (data) {
+
 		//get hash (e.g. pghforensics/#2017)
 		var hash = window.location.hash.substring(1);
 		var yearData;
-		if(hash !== ""){
+		if (hash !== "") {
 			for (var i = data.length - 1; i >= 0; i--) {
-				if(data[i].year == hash)
+				if (data[i].year == hash)
 					yearData = data[i];
 			}
-			if(yearData == undefined)
+			if (yearData == undefined)
 				yearData = data[data.length - 1];
-		}else
+		} else
 			yearData = data[data.length - 1]; //get data for current year
 
 		//assign signup link to button
